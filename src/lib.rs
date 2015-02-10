c5c30e329b966170ee852b59175ea8e5edc78a01
Philipp Oppermann <dev@phil-opp.com>
2015-02-10 15:22:08 +0100

    add #![no_std] again (with `feature(no_std)`)

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@
 
 //! A typesafe bitmask flag generator.
 
+#![no_std]
+#![feature(no_std)]
 /// The `bitflags!` macro generates a `struct` that holds a set of C-style
 /// bitmask flags. It is useful for creating typesafe wrappers for C APIs.
 ///
